d3b30427415d2d9bc1ac6cc379911fadee0f3549
Tatyana Smulskaya <tatyana.wrk@gmail.com>
2017-03-12 13:18:25 +0200

    Ответ на задание №2: Потренироваться использовать функции, классы, объекты и методы

---
+++ sandbox/src/main/java/stqa/Task2.java
@@ -0,0 +1,17 @@
+package stqa;
+
+/**
+ * Created by tatyanas on 3/11/17.
+ */
+public class Task2 {
+
+  public static void main(String[] args){
+
+    Point p1 = new Point(3, 3);
+    Point p2 = new Point(6, 6);
+    System.out.println("Расстояние между заданными точками равно " + p1.distance(p2));
+
+  }
+
+
+}
